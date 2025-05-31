681dd4a5561d065e6259822c9463994b826f673b
Kilari Teja <kilariteja9@gmail.com>
2025-05-31 18:57:10 +0530

    x: Add basic dino

--- src/bin/dino.rs
+++ src/bin/dino.rs
@@ -15,7 +15,7 @@ async fn main(_spawner: Spawner) {
 
     // Config is 
     let mut config = Config::default();
-    config.frequency = 400_000;
+    config.frequency = 1_000_000;
 
     // TODO: async :)
     let mut i2c = I2c::new_blocking(p.I2C1, p.PIN_19, p.PIN_18, config);
@@ -29,11 +29,37 @@ async fn main(_spawner: Spawner) {
     screen.init().expect("failed screen init");
     screen.flush().expect("failed screen flush");
 
-    // Test.
-    screen.set_pixel(10, 10, true);
+    // (6, 10) in https://www.reddit.com/r/PixelArt/comments/kzqite/oc_cute_8x8_pixel_art_with_max_3_colours_per/#lightbox
+    let dino: [bool; 64] = [
+        false, true,  true, true,  true, false, false, false,
+        true,  false, true, false, true, false, false, false,
+        true,  true,  true, true,  true, false, false, false,
+        false, false, true, true,  true, false, false, false,
+        false, true,  true, true,  true, false, true,  false,
+        false, false, true, true,  true, false, true,  false,
+        false, false, true, true,  true, true,  false, false,
+        false, false, true, false, true, false, false, false,
+    ];
+
+    screen.clear_buffer();
+
+    screen.set_pixel(0, 0, true);
+    screen.set_pixel(1, 1, true);
+
+    for y in 0..8 {
+        for x in 0..8 {
+            screen.set_pixel(x + 10, y + 10, dino[(y*8 + (7 - x)) as usize]);
+        }
+    }
     screen.flush().expect("failed screen flush");
 
+    // Test.
+    let mut base_y = 0;
     loop {
-        Timer::after_secs(1).await;
+
+        base_y += 1;
+        if base_y > 64 {
+            base_y = 0;
+        }
     }
 }
