9067c9738c26d331c8144e1d11a222edd8a5e149
Kilari Teja <kilariteja9@gmail.com>
2025-05-31 19:49:06 +0530

    WIP

---
+++ src/dino.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+use defmt::{info};
+use defmt_rtt as _;
+use embassy_rp::i2c::{Config, I2c};
+use embassy_executor::{self, Spawner};
+use ssd1306::{mode::DisplayConfig, prelude::DisplayRotation, size::{DisplaySize128x64}, I2CDisplayInterface};
+use {panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    // Config with Fast Mode Plus.
+    let mut config = Config::default();
+    config.frequency = 1_000_000;
+
+    // TODO: async :)
+    let mut i2c = I2c::new_blocking(p.I2C1, p.PIN_19, p.PIN_18, config);
+
+    // Screen
+    let mut interface = I2CDisplayInterface::new(i2c);
+    let mut screen = ssd1306::Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+
+    // Init screen.
+    screen.init().expect("failed screen init");
+    screen.flush().expect("failed screen flush");
+
+    loop {
+    }
+}
